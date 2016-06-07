d4c9ed3adbc6fd182b42f6f8a354a39552a7dccd
sdavidf <scottdavidfolsom@gmail.com>
2016-06-07 13:23:22 -0500

    reads data from file, not hard-coded

--- coffeebot.py
+++ coffeebot.py
@@ -1,10 +1,10 @@
-#coffeebot.py
-#post nonsense coffee orders to a twitter
-#06.06.16
-
+#coffeebot2.py
+#new coffeebot, reads from text files
+#06.07.2016
 import random
 from twython import Twython
 
+#initial stuff, needs refactored into something neater...
 APP_KEY = ""
 APP_SECRET = ""
 OAUTH_TOKEN = ""
@@ -12,29 +12,37 @@ OAUTH_TOKEN_SECRET = ""
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
+(size, flavor, attribute, topping) = [], [], [], []
 
-def coffee():
-	#these lists are under constant revision
-    sizes = ["tall", "grande", "venti", "trenta"]
-    attributes = ["soy", "half-caff", "decaf", "iced", "double-shot"]
-    drinktype = ["latte", "frappucino", "tea", "Americano", "Mocha"]
-    whip = ["with whip", "no whip", "with foam"]
-	#yeah, foam != whip, but this is the place where a request for foam is
-	#most syntactically valid
-	#random generator
-    random.seed()
-    #randomize lists
-    random.shuffle(sizes)
-    random.shuffle(attributes)
-    random.shuffle(drinktype)
-    random.shuffle(whip)
-    #pop the first element
-    s = sizes[1]
-    a = attributes[1]
-    d = drinktype[1]
-    w = whip[1]
-	#push out order. TODO: Make it ruder.
-    twitter.update_status(status="#coffeebot Give me a %s %s %s, %s." % (s, a, d, w))
-    print "OK"
 
-coffee()
+def assign():
+    #reads drink names from file, separates data from code
+    global size
+    global flavor
+    global attribute
+    global topping
+
+    s = open("sizes.txt")
+    f = open("flavors.txt")
+    a = open("attributes.txt")
+    t = open("toppings.txt")
+
+    size = s.read().splitlines()
+    flavor = f.read().splitlines()
+    attribute = a.read().splitlines()
+    topping = t.read().splitlines()
+
+def post():
+    #shuffles lists, makes post (not modular enough!!!)
+    random.shuffle(size)
+    random.shuffle(attribute)
+    random.shuffle(flavor)
+    random.shuffle(topping)
+
+    #push out order. TODO: Make it ruder.
+    twitter.update_status(status="#coffeebot Give me a %s %s %s, with %s."
+        % (size[1], flavor[1], attribute[1], topping[1]))
+
+
+assign()
+post()
